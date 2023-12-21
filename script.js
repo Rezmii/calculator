0e49aff275973650c2bf32353730c8aaa5d0a814
Rezmii <bartoszrezmer20@gmail.com>
2023-12-21 14:40:29 +0100

    added function that combines all operations

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let num1, num2;
+let operator;
+
 function addNumbers(num1, num2) {
   return (result = num1 + num2);
 }
@@ -12,7 +15,27 @@ function divideNumbers(num1, num2) {
   else return (result = num1 / num2);
 }
 
-console.log(addNumbers(4, 2));
-console.log(subtractNumbers(4, 2));
-console.log(multiplyNumbers(4, 2));
-console.log(divideNumbers(4, 0));
+function chooseOperation(num1, num2, operator) {
+  let result;
+  switch (operator) {
+    case "+":
+      result = addNumbers(num1, num2);
+      break;
+    case "-":
+      result = subtractNumbers(num1, num2);
+      break;
+    case "*":
+      result = multiplyNumbers(num1, num2);
+      break;
+    case "/":
+      result = divideNumbers(num1, num2);
+      break;
+    default:
+      result = "There is no such operator";
+      break;
+  }
+
+  return result;
+}
+
+console.log(chooseOperation(3, 5, "+"));
